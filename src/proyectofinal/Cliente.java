3815427858d2df754ed56ddd5faded3948e3db19
FabianArias <f.ariasartavia09@gmail.com>
2023-06-26 10:04:04 -0600

    Se añadieron los metodos crud en cliente

--- src/proyectofinal/Cliente.java
+++ src/proyectofinal/Cliente.java
@@ -35,22 +35,176 @@ public class Cliente extends Persona implements CrudInterface{
 
     @Override
     public void registrar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //Se crea un boolean que nos ayudara al try catch
+        boolean valorCorrecto = false;
+        
+        while(true){
+            String salir = JOptionPane.showInputDialog
+        ("Digite SALIR para terminar - otra cosa para seguir").toUpperCase();
+            
+            if("SALIR".equals(salir)){
+                //quebrar el ciclo
+                break;
+            }//fin del if;  
+            
+            //ingresar informacion del empleado
+            super.setNombre(JOptionPane.showInputDialog
+            ("Digite el nombre completo del cliente"));
+            
+            while(!valorCorrecto){
+                try {
+                super.setEdad(Integer.parseInt(JOptionPane.showInputDialog
+                ("Digite la edad del cliente")));
+                valorCorrecto = true;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog
+                (null, "Valor incorrecto");
+                }
+            }
+            
+            valorCorrecto = false; //se reinicia el valor del boolean
+            
+            while(!valorCorrecto){
+                try {
+                    super.setID(Integer.parseInt(JOptionPane.showInputDialog
+                    ("Digite la cedula del cliente")));
+                    valorCorrecto = true;
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Valor incorrecto");
+                }
+            }
+            valorCorrecto = false; //se reinicia el valor del boolean
+            
+            correoElectronico = JOptionPane.showInputDialog("Digite el correo electronico del cliente");
+            
+            listaClientes.add(new Cliente(correoElectronico, super.getID(), super.getNombre(), super.getEdad()));
+        }
     }
 
     @Override
     public void modificar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        boolean valorCorrecto = false;
+        int cedula;
+        int opc;
+        
+        //se solicita la cedula para buscar el empleado
+        cedula = Integer.parseInt(JOptionPane.showInputDialog(null, 
+        "Digite el ID de la persona que quieres modificar en el programa"));
+        
+        //se recorre el arraylist buscando la cedula de la persona a modificar
+        for(int i=0; i<listaClientes.size(); i++){
+            if(cedula == listaClientes.get(i).getID()){
+                //creamos un mini menu para seleccionar la opcion deseada
+                opc = Integer.parseInt(JOptionPane.showInputDialog
+                (null, "Ingrese lo que desea hacer\n" 
+                + "\n"
+                + "1. Editar Cedula\n"
+                + "\n"
+                + "2. Editar Nombre\n"
+                + "\n"
+                + "3. Editar edad\n"
+                + "\n"
+                + "4. Editar Correo Electronico\n"
+                + "\n"
+                + "5. Salir del sistema\n")); 
+                switch(opc){
+        //en el caso 1 editamos la cedula            
+        case 1:{
+            //creamos una variable nueva para almacenar la cedula nueva
+            int cedulaNueva = 0;
+            
+            while(!valorCorrecto){
+                //creamos un try catch para evitar que se caiga el programa
+                try {
+                    cedulaNueva = Integer.parseInt
+                    (JOptionPane.showInputDialog("Digite la cedula"));
+                    valorCorrecto = true;
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Valor Incorrecto");
+                }
+            }            
+            //Le devolvemos el valor a false para que siga funcionando a futuro
+            valorCorrecto = false;
+            
+            //guardamos la variable nueva en donde estaba la cedula anterior
+            listaClientes.get(i).setID(cedulaNueva);
+            consultar();
+            break;
+                    }
+        //en el caso 2 editamos el nombre
+        case 2:{
+            listaClientes.get(i).setNombre
+            (JOptionPane.showInputDialog("Escriba el nombre del empleado"));
+            consultar();
+            break;
+                    }
+        //en el caso 3 editamos la edad
+        case 3:{
+             //creamos una variable nueva para almacenar la cedula nueva
+            int edadNueva = 0;
+            
+            while(!valorCorrecto){
+                //creamos un try catch para evitar que se caiga el programa
+                try {
+                    edadNueva = Integer.parseInt
+                    (JOptionPane.showInputDialog("Digite la edad"));
+                    valorCorrecto = true;
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Valor Incorrecto");
+                }
+            }
+            //Le devolvemos el valor a false para que siga funcionando a futuro
+            valorCorrecto = false;
+            
+            //guardamos la variable nueva en donde estaba la edad anterior
+            listaClientes.get(i).setEdad(edadNueva);
+            consultar();
+            break;
+                    }
+        //en el caso 4 editamos el correo electronico
+        case 4:{
+            listaClientes.get(i).setCorreoElectronico
+            (JOptionPane.showInputDialog("Ingrese el correo electronico del empleado"));
+            consultar();
+            break;
+                    }
+        case 5:{
+            System.exit(0);
+                    }
+        default:{
+            JOptionPane.showMessageDialog(null,
+                    "Opcion no valida");
+            }
+                }
+            }
+        }
     }
 
     @Override
     public void consultar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        String msj = "";
+        for(Cliente a: listaClientes){
+            msj = msj +"\n" + a;
+        }//fin del for
+        
+        JOptionPane.showMessageDialog(null, msj);
+        }
 
     @Override
     public void eliminar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int cedula;
+        
+        //se solicita la cedula a eliminar
+        cedula = Integer.parseInt(JOptionPane.showInputDialog(null, 
+        "Digite el ID del cliente a eliminar"));
+        
+        //se recorre el arraylist buscando la cedula a eliminar
+        for(int i=0; i<listaClientes.size(); i++){
+            if(cedula == listaClientes.get(i).getID()){
+                listaClientes.remove(i);    
+            }
+        }
+    }
     }
     
 }//fin de la clase
